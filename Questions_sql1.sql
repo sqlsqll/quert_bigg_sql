3dc2708da8cb6d6c05b8bf251e47dfda18dda262
sqlsqll <100778968+sqlsqll@users.noreply.github.com>
2022-03-04 12:57:13 +0530

    Update and rename Questions_sql1 to Questions_sql1.sql

--- Questions_sql1.sql
+++ Questions_sql1.sql
@@ -1,4 +1,4 @@
-1.	India use which metrics to measure the pollution?
+#1.	India use which metrics to measure the pollution?
 
 SELECT metric_used, COUNT(metric_used) AS Total_used
 FROM `bigquery-public-data.epa_historical_air_quality.air_quality_annual_summary`
