40f73f79d7bdba506138989e82f63bfbd6cf30b6
sqlsqll <100778968+sqlsqll@users.noreply.github.com>
2022-03-04 12:59:42 +0530

    Update and rename Questions_sql3 to Questions_sql3.sql

--- Questions_sql3.sql
+++ Questions_sql3.sql
@@ -1,4 +1,4 @@
-Which scientific methods did India employ for the case of measuring pollution?
+#3.Which scientific methods did India employ for the case of measuring pollution?
 SELECT method_name, COUNT(method_name) AS Most_used_method
 FROM `bigquery-public-data.epa_historical_air_quality.air_quality_annual_summary`
 WHERE county_code LIKE "%91%"
