31334b39ac9d8aba3bd48f976119e9de47ba8c2c
sqlsqll <100778968+sqlsqll@users.noreply.github.com>
2022-03-04 13:14:25 +0530

    Create Questions_sql5.sql

---
+++ Questions_sql5.sql
@@ -0,0 +1,4 @@
+#5.What is the average pollution in  the globe during 2017?
+select avg(observation_count) as average_pollution
+from `bigquery-public-data.epa_historical_air_quality.air_quality_annual_summary`
+where year=(2017)
