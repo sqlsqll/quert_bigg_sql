aece9cff727b2f6235978b0353497a1d44d8f556
sqlsqll <100778968+sqlsqll@users.noreply.github.com>
2022-03-04 12:59:13 +0530

    Update and rename Questions_sql2 to Questions_sql2.sql

--- Questions_sql2.sql
+++ Questions_sql2.sql
@@ -1,4 +1,4 @@
- India used which Pollution standard in measuring pollution?
+#2. India used which Pollution standard in measuring pollution?
  
 SELECT pollutant_standard, COUNT(pollutant_standard) AS Most_used_standard
 FROM `bigquery-public-data.epa_historical_air_quality.air_quality_annual_summary`
