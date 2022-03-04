ec845dc6d4f67913520cb301fc42e3baf94b5d57
sqlsqll <100778968+sqlsqll@users.noreply.github.com>
2022-03-04 22:55:12 +0530

    Create Questions_sql8.sql

---
+++ Questions_sql8.sql
@@ -0,0 +1,5 @@
+#8. INJOIN
+SELECT  aa.County_of_Residence
+FROM `bigquery-public-data.sdoh_cdc_wonder_natality.county_natality_by_father_race` aa
+INNER JOIN `bigquery-public-data.sdoh_cdc_wonder_natality.county_natality_by_congenital_abnormalities` bb
+ON aa.County_of_Residence= bb.County_of_Residence
