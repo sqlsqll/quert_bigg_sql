44eff7b1eb6c33cfd1d4e55999237036b853fb22
sqlsqll <100778968+sqlsqll@users.noreply.github.com>
2022-03-04 16:58:50 +0530

    Update Questions_sql6.sql

--- Questions_sql6.sql
+++ Questions_sql6.sql
@@ -1,4 +1,4 @@
-# 1. What is the most common type of crime in streets of Chicago?
+## 6. What is the most common type of crime in streets of Chicago?#
 SELECT  primary_type, COUNT(primary_type) AS  Crime_type
 FROM `bigquery-public-data.chicago_crime.crime`
 WHERE location_description  LIKE "%STREET%"
