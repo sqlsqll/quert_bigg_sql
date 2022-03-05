39728dcfc8d33c4057815e6ada2c20dcd0e9861a
sqlsqll <100778968+sqlsqll@users.noreply.github.com>
2022-03-05 11:09:45 +0530

    Update Questions_sql9.sql

--- Questions_sql9.sql
+++ Questions_sql9.sql
@@ -1,5 +1,9 @@
-# 9.
-SELECT  aa.country_code
+# 9. Which group of countries has the highest education rate?
+SELECT  aa.country_name,sum(aa.value) as count
 FROM `bigquery-public-data.world_bank_intl_education.international_education` aa
 LEFT JOIN `bigquery-public-data.world_bank_intl_education.country_summary`  bb
 ON aa.country_code= bb.country_code
+where country_name not like "%income%" and country_name not like"%World%"
+group by 1
+order by 2 desc 
+
