aca7f0f7e8fccccfe2e1c4497eb5ed0f673fa4bb
sqlsqll <100778968+sqlsqll@users.noreply.github.com>
2022-03-05 12:45:03 +0530

    Update Questions_sql10.sql

--- Questions_sql10.sql
+++ Questions_sql10.sql
@@ -1,6 +1,8 @@
-#10.How is the diameter of the tree different in 2005 census as compared to 1995?
+#10.How is the diameter of the tree different among trees of different statuses?
 SELECT 
-*
+aa.STATUS,AVG(diameter) AS AVG_DIA
 FROM `bigquery-public-data.new_york.tree_census_1995` as aa 
 JOIN `bigquery-public-data.new_york.tree_census_2015` as bb  
-ON (aa.recordid=bb.tree_id)
+ON aa.recordid=bb.tree_id
+GROUP BY 1
+ORDER BY 2 desc 
